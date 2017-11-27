43c6763baa7e6f44a0bccd7a0ea80b9cd3662832
Vivek Kailasnath <vivek@cpcdiagnostics.in>
2017-11-27 13:33:54 +0530

    applied pep-8 style guide to entertainment_center.py and comments for media.py

--- media.py
+++ media.py
@@ -4,12 +4,23 @@ import webbrowser
 
 class Movie():
     def __init__(self, _title, _storyline, _poster, _youtube):
+        '''
+        CONSTRUCTOR OF THE MOVIE CLASS
+        CONTAINS THE FOLLOWING ELEMENTS
+        title - String
+        storyline - String
+        poster_image_url - String
+        trailer_youtube_url - String
+        '''
         self.title = _title
         self.storyline  = _storyline
         self.poster_image_url = _poster
         self.trailer_youtube_url = _youtube
 
     def ShowTrailer():
+        '''
+        OPENS THE TRAILER YOUTUE CHANNEL IN A WEBBROWSER OBJECT
+        '''
         webbrowser.open(self.trailer_youtube_url)
 
 if __name__ == '__main__':
